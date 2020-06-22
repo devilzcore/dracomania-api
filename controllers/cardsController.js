2ffde7d8b3e6208e2b27a2b2a79c330d7059c98b
devilzcore <aldosilva1@live.com>
2020-06-22 17:07:26 -0300

    Add increment count mongoose

--- controllers/cardsController.js
+++ controllers/cardsController.js
@@ -1,48 +1,58 @@
-const db = require("../models");
-const Cards = db.cards;
+const db = require("../models")
+const Cards = db.cards
 
+// Create and Save a new cards
 exports.create = (req, res) => {
-
+ // Validate request
  if (!req.body.name) {
-    res.status(400).send({ message: "Content can not be empty!" });
-    return;
+    res.status(400).send({ message: "Content can not be empty!" })
+    return
   }
 
+  // Create a cards
   const cards = new Cards({
     img: req.body.img,
     name: req.body.name,
     magic: req.body.magic,
     str: req.body.str,
-    fire: req.body.fire
-  });
+    fire: req.body.fire,
+  })
 
+  // Save cards in the database
   cards
     .save(cards)
     .then(data => {
-      res.send(data);
+      res.send(data)
     })
     .catch(err => {
       res.status(500).send({
         message:
           err.message || "Some error occurred while creating the cards."
-      });
-    });  
-};
+      })
+    })  
+}
 
 exports.findAll = (req, res) => {
-};
 
-exports.findOne = (req, res) => { 
-};
+}
 
-exports.update = (req, res) => {  
-};
+exports.findOne = (req, res) => {
 
-exports.delete = (req, res) => {  
-};
+}
+
+exports.update = (req, res) => {
+
+}
+
+exports.delete = (req, res) => {
+
+}
 
 exports.deleteAll = (req, res) => {
-};
+
+}
+
 
 exports.findAllPublished = (req, res) => {
-};
+
+}
--- models/cardsModel.js
+++ models/cardsModel.js
@@ -3,19 +3,35 @@ const { urlencoded } = require("body-parser")
 module.exports = mongoose => {
     var schema = mongoose.Schema(
       {
+        _id: Number,
         img: String,
         name: String,
         magic: Number,
         str: Number,
         fire: Number
-      })
-  
+      },
+
+      { versionKey: false }
+    )
+
     schema.method("toJSON", function() {
       const { __v, _id, ...object } = this.toObject()
       object.id = _id 
       return object 
     })
-  
+
+    schema.pre('save', function (next) {
+      // Only increment when the document is new
+      if (this.isNew) {
+        Cards.count().then(res => {
+          this._id = res // Increment count
+          next()
+        })
+      } else {
+        next()
+      }  
+    })
+    
     const Cards = mongoose.model("cards", schema)
     return Cards
   }
