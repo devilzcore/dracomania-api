09d79b504a45ddb082d2b5895b58aebadb20e362
devilzcore <aldosilva1@live.com>
2020-06-24 22:21:55 -0300

    Add exports in Controller

--- controllers/cardsController.js
+++ controllers/cardsController.js
@@ -32,27 +32,114 @@ exports.create = (req, res) => {
     })  
 }
 
+// Retrieve all cards from the database.
 exports.findAll = (req, res) => {
-
+    const name = req.query.name
+    var condition = name ? { name: { $regex: new RegExp(name), $options: "i" } } : {}
+  
+    Cards.find(condition)
+      .then(data => {
+        res.send(data)
+      })
+      .catch(err => {
+        res.status(500).send({
+          message:
+            err.message || "Some error occurred while retrieving cards."
+        })
+      })
 }
 
+// Find a single cards with an id
 exports.findOne = (req, res) => {
+    const id = req.params.id
 
+    Cards.findById(id)
+      .then(data => {
+        if (!data)
+          res.status(404).send({ message: "Not found card with id " + id })
+        else res.send(data)
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .send({ message: "Error retrieving card with id=" + id })
+      })
 }
 
+// Update a cards by the id in the request
 exports.update = (req, res) => {
-
+    if (!req.body) {
+        return res.status(400).send({
+          message: "Data to update can not be empty!"
+        })
+      }
+    
+      const id = req.params.id
+    
+      Cards.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+        .then(data => {
+          if (!data) {
+            res.status(404).send({
+              message: `Cannot update card with id=${id}. Maybe card was not found!`
+            })
+          } else res.send({ message: "Card was updated successfully." })
+        })
+        .catch(err => {
+          res.status(500).send({
+            message: "Error updating card with id=" + id
+          })
+        })
 }
 
+// Delete a cards with the specified id in the request
 exports.delete = (req, res) => {
+    const id = req.params.id
 
+    Cards.findByIdAndRemove(id)
+      .then(data => {
+        if (!data) {
+          res.status(404).send({
+            message: `Cannot delete card with id=${id}. Maybe card was not found!`
+          })
+        } else {
+          res.send({
+            message: "Card was deleted successfully!"
+          })
+        }
+      })
+      .catch(err => {
+        res.status(500).send({
+          message: "Could not delete card with id=" + id
+        })
+      }) 
 }
 
+// Delete all cardss from the database.
 exports.deleteAll = (req, res) => {
-
+    Cards.deleteMany({})
+    .then(data => {
+      res.send({
+        message: `${data.deletedCount} Cards were deleted successfully!`
+      })
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while removing all cards."
+      })
+    })
 }
 
-
+// Find all published cardss
 exports.findAllPublished = (req, res) => {
-
+    Cards.find({ published: true })
+    .then(data => {
+      res.send(data)
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving cards."
+      })
+    })
 }
--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,21 @@
 module.exports = app => {
-    const cards = require("../controllers/cardsController");
-  
-    var router = require("express").Router();
-  
-    router.post("/", cards.create);
-  
-    router.get("/", cards.findAll);
-  
-    router.get("/published", cards.findAllPublished);
-  
-    router.get("/:id", cards.findOne);
-  
-    router.put("/:id", cards.update);
-  
-    router.delete("/:id", cards.delete);
-  
-    router.delete("/", cards.deleteAll);
-  
-    app.use('/api/cards', router);
-  };
+  const cards = require("../controllers/cardsController");
+
+  var router = require("express").Router();
+
+  router.post("/", cards.create);
+
+  router.get("/", cards.findAll);
+
+  router.get("/published", cards.findAllPublished);
+
+  router.get("/:id", cards.findOne);
+
+  router.put("/:id", cards.update);
+
+  router.delete("/:id", cards.delete);
+
+  router.delete("/", cards.deleteAll);
+
+  app.use('/api/cards', router);
+};
